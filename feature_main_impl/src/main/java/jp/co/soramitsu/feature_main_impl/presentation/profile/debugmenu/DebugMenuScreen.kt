1ee25eba11743ba4487463670c4f074898f82592
arvifox <arvifox@hotmail.com>
2023-08-02 12:28:13 +0300

    sca-65

--- feature_main_impl/src/main/java/jp/co/soramitsu/feature_main_impl/presentation/profile/debugmenu/DebugMenuScreen.kt
+++ feature_main_impl/src/main/java/jp/co/soramitsu/feature_main_impl/presentation/profile/debugmenu/DebugMenuScreen.kt
@@ -33,17 +33,17 @@ USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 package jp.co.soramitsu.feature_main_impl.presentation.profile.debugmenu
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -58,7 +58,7 @@ internal fun DebugMenuScreen(
     LazyColumn(
         modifier = Modifier.fillMaxSize(),
         contentPadding = PaddingValues(vertical = Dimens.x1),
-        verticalArrangement = Arrangement.spacedBy(Dimens.x2)
+        verticalArrangement = Arrangement.spacedBy(Dimens.x1)
     ) {
         items(
             items = state.settings
@@ -69,23 +69,18 @@ internal fun DebugMenuScreen(
                 ).fillMaxSize(),
                 innerPadding = PaddingValues(Dimens.x2)
             ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
+                Column(
+                    modifier = Modifier.fillMaxWidth().wrapContentHeight()
                 ) {
                     Text(
                         text = item.name,
-                        style = MaterialTheme.customTypography.textM,
+                        style = MaterialTheme.customTypography.textXSBold,
                         maxLines = 1,
                         overflow = TextOverflow.Ellipsis,
-                    )
-                    Spacer(
-                        Modifier.weight(1.0F)
                     )
                     Text(
                         text = item.value,
                         style = MaterialTheme.customTypography.textM,
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis,
                     )
                 }
             }
@@ -103,7 +98,7 @@ internal class SettingOption(
 private fun PreviewOptions() {
     val settingsList: List<SettingOption> = mutableListOf<SettingOption>().apply {
         add(SettingOption(name = "User ID", value = "_ID"))
-        add(SettingOption(name = "Device Name", value = "Some Device"))
+        add(SettingOption(name = "Device Name", value = "Some Device Very Big Value Text More And More Letters"))
     }
 
     DebugMenuScreen(DebugMenuScreenState(settings = settingsList))
